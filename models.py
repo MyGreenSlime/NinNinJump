d5268d42d54e67a8c583f012707d17e963e9ce1f
DONDON <ruksachai.o@ku.th>
2017-10-05 05:02:42 +0700

    add barrel

--- models.py
+++ models.py
@@ -1,4 +1,6 @@
 import arcade.key
+from random import randint
+
 class Ninja:
     def __init__(self,world,x,y):
         self.world = world
@@ -8,9 +10,9 @@ class Ninja:
         self.vx = 0
     def direction(self):
         if self.move == 0:
-            self.vx = -5
+            self.vx = -16
         elif self.move == 1:
-            self.vx = 5
+            self.vx = 16
     def update(self,delta):
         self.x+=self.vx
         if(abs(600-self.x)<=32):
@@ -21,15 +23,30 @@ class Ninja:
             self.x = 32
             self.vx = 0
             self.move = 1
+class Item():
+    def __init__(self,world,x,y):
+        self.pos = [568,32]
+        self.x = self.pos[randint(0,1)]
+        self.y = 1200
+    def random_location(self):
+        self.x = self.pos[randint(0,1)]
+        self.y = 1200
+    def update(self,delta):
+        self.y-=5
 class World:
     def __init__(self,width,height):
         self.width = width
         self.height  =height
         self.ninja = Ninja(self,568,300)
+        self.barrel = Item(self,0,0)
     def on_key_press(self, key, key_modifiers):
         if key == arcade.key.SPACE:
             self.ninja.direction()
     def update(self,delta):
         self.ninja.update(delta)
+        self.barrel.update(delta)
+
+        if(self.barrel.y<0):
+            self.barrel.random_location()
     
         
--- ninninjump.py
+++ ninninjump.py
@@ -19,19 +19,21 @@ class ModelSprite(arcade.Sprite):
 class NinninWindow(arcade.Window):
     def __init__(self,width,height):
         super().__init__(width,height)
-        #self.background = None
+        self.background = None
         arcade.set_background_color(arcade.color.AMAZON)
-        #self.background = arcade.load_texture("images/background.png")
+        self.background = arcade.load_texture("images/background.png")
         self.world = World(width,height)
         self.ninja = ModelSprite('images/char1.png',model = self.world.ninja)
+        self.barrel = ModelSprite('images/item1.png',model = self.world.barrel)
     def on_key_press(self, key, key_modifiers):
         self.world.on_key_press(key, key_modifiers)
     def update(self,delta):
         self.world.update(delta)
     def on_draw(self):
         arcade.start_render()
-        #arcade.draw_texture_rectangle(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2,SCREEN_WIDTH, SCREEN_HEIGHT, self.background)
+        arcade.draw_texture_rectangle(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2,SCREEN_WIDTH, SCREEN_HEIGHT, self.background)
         self.ninja.draw()
+        self.barrel.draw()
 
 if __name__ == '__main__':
     window =NinninWindow(SCREEN_WIDTH,SCREEN_HEIGHT)
