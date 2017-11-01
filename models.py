cbb7a51bd86f0277392b5d97fe1549b0f488977b
DONDON <ruksachai.o@ku.th>
2017-11-02 00:22:19 +0700

    add comment code

--- models.py
+++ models.py
@@ -22,15 +22,15 @@ class Ninja:
         if((abs(self.x - other.x)<=hit_sizex)and(abs(self.y-other.y)<=hit_sizey)):
             return True
         else: 
-            False
+            return False
     def Die(self):
-        self.vy =-10
+        self.vy =-15
     def update(self,delta):
         self.y+=self.vy
         self.x+=self.vx
         if(self.y<0):
             self.status = 0
-        if(abs(400-self.x)<=32):
+        if(abs(400-self.x)<=32): 
             self.x = 368
             self.vx = 0
             self.move = 0
@@ -59,21 +59,17 @@ class Item():
         self.x = -100
         self.y = randint(1000,1020)
         self.angle = 0        
-        self.posx = [32,368] 
+        self.posx = [32,368] #position x axis 
         self.move = [10,10]
         self.speed = self.move[randint(0,1)]
-        self.sleep = 0
-        self.time= 0
     def random_location(self,range1,range2):
-        self.time = 0
-        self.rand = randint(range1,range2)
+        self.rand = randint(range1,range2) #random for check item is droping
         if(self.rand == 0):
-            self.x = self.posx[randint(0,1)]
+            self.x = self.posx[randint(0,1)] #random position x axis
         else:
             self.x = -100
         self.y = randint(1000,1020)
         self.speed = self.move[randint(0,1)]
-        self.sleep = 0
     def cancel(self):
         self.x  = -100
     def setspeed(self,A,B):
@@ -82,31 +78,34 @@ class Item():
             self.y -= self.speed
 class World:
     TIMECHANGE = 0.075
-    def __init__(self,width,height):
-        self.width = width
-        self.height  =height
+    def __init__(self):
         self.addscore = 1
         self.score = 0
+        self.time = 0 #time for random items drop
+        self.timepic = 0 #time for change pic
+        self.check =0
+        self.flip = 0
+        self.limitscore = 2500 #point of change level
+        #speed of item 
+        self.speeditem1 =11
+        self.speeditem2 =12
+
+        self.live = 1
+
         self.ninja = Ninja(self,368,100,0)
         self.sheild = Sheild(self,368,100,0)
+
         self.barrel = Item(self,0,0,0)
         self.shuriken1 = Item(self,0,0,0)
         self.knife = Item(self,0,0,0)
         self.gensheild = Item(self,0,0,0)
         self.barrel2 = Item(self,0,0,0)
-        self.time = 0
-        self.timepic = 0
-        self.check =0
+        #list of items
         self.item1 = [self.barrel,self.shuriken1,self.knife,self.barrel2]
-        self.flip = 0
-        self.limitscore = 2500
-        self.speeditem1 =11
-        self.speeditem2 =12
-        self.live = 1
     def on_key_press(self, key, key_modifiers):
         if key == arcade.key.SPACE and self.live != 0:
             self.ninja.direction()
-            if(self.ninja.x==32 or self.ninja.x==368):
+            if(self.ninja.x==32 or self.ninja.x==368): #flip character
                 self.flip+=1
                 self.flip%=2
     def update(self,delta):
@@ -115,36 +114,25 @@ class World:
         self.timepic += delta
         self.ninja.update(delta)
         self.gensheild.update(delta)
-        if(self.gensheild.y<0):
+        if(self.gensheild.y<0): #generate sheild
             self.gensheild.random_location(0,15)
-        if(self.live == 2):
+
+        self.sheild.update(delta)
+        if(self.live == 2): #set postion of sheild
             if(self.flip==1):
                 self.sheild.position(self.ninja.x+15,self.ninja.y)
             else: 
                 self.sheild.position(self.ninja.x-15,self.ninja.y)
-        self.sheild.update(delta)
-        for i in self.item1:
-            i.update(delta)
-        if(self.score>=self.limitscore):
-            for i in self.item1:
-                i.setspeed(self.speeditem1,self.speeditem2)
-            self.gensheild.setspeed(self.speeditem1,self.speeditem2)
-            self.speeditem1+=2
-            self.speeditem2+=2
-            self.limitscore+=2500
-            self.ninja.speed+=2
+        
         j = 0
         timeset = [0.1,0.4,0.7,1,1.3]
         for i in self.item1:
-            if(i.y<0 and (self.time%2 >=timeset[j] and self.time%2<=timeset[j+1]-0.2)):
+            i.update(delta)
+        for i in self.item1: 
+            if(i.y<0 and (self.time%2 >=timeset[j] and self.time%2<=timeset[j+1]-0.2)): #random items drop
                 i.random_location(0,1)
             j+=1
-        if(self.timepic>=World.TIMECHANGE):
-            self.ninja.pic+=1
-            self.ninja.pic%=6   
-            self.timepic = 0   
-        for i in self.item1:
-            if(self.ninja.onhit(i,50,70)==True):
+            if(self.ninja.onhit(i,50,70)==True): #check hit and die
                 self.live-=1
                 if(self.live == 1):
                     self.sheild.cancel()
@@ -157,7 +145,22 @@ class World:
                     self.ninja.Die()
                     self.addscore =0
                 break 
-        if(self.ninja.onhit(self.gensheild,50,70)==True):
+
+        if(self.score>=self.limitscore): #change level
+            for i in self.item1:
+                i.setspeed(self.speeditem1,self.speeditem2)
+            self.gensheild.setspeed(self.speeditem1,self.speeditem2)
+            self.speeditem1+=2
+            self.speeditem2+=2
+            self.limitscore+=2500
+            self.ninja.speed+=2
+        
+        if(self.timepic>=World.TIMECHANGE): #change pic of character
+            self.ninja.pic+=1
+            self.ninja.pic%=6   
+            self.timepic = 0   
+        
+        if(self.ninja.onhit(self.gensheild,50,70)==True): #check hit sheild
             self.gensheild.cancel()
             if(self.live<=1):
                 self.live+=1
--- ninninjump.py
+++ ninninjump.py
@@ -22,32 +22,38 @@ class NinninWindow(arcade.Window):
     def __init__(self,width,height):
         super().__init__(width,height)
         self.background = None
+        arcade.set_background_color(arcade.color.AMAZON)
         self.wait_time = 0
         self.rand = 0
         self.setninja1 = ['images/char1.png','images/char2.png','images/char3.png','images/char4.png','images/char5.png','images/char6.png']
         self.setninja2 = ['images/char1flip.png','images/char2flip.png','images/char3flip.png','images/char4flip.png','images/char5flip.png','images/char6flip.png']
         self.setninja = [self.setninja1,self.setninja2]
         self.setsheild = ['images/sheild.png','images/sheildflip.png']
-        arcade.set_background_color(arcade.color.AMAZON)
         self.background = arcade.load_texture("images/background.png")
-        self.world = World(width,height)
+        self.world = World()
+        #character
         self.ninja = ModelSprite(self.setninja[0][0],model = self.world.ninja)
         self.sheild = ModelSprite(self.setsheild[0],model = self.world.sheild)
+        #item
         self.gensheild = ModelSprite('images/item4.png',model = self.world.gensheild)
         self.barrel = ModelSprite('images/item1.png',model = self.world.barrel)
         self.barrel2 = ModelSprite('images/item1.png',model = self.world.barrel2)
         self.shuriken = ModelSprite('Images/item2.png',model = self.world.shuriken1)
         self.knife = ModelSprite('images/item3.png',model = self.world.knife)
+        #text
         self.textscore = self.ReadScore()
-    def setup(self):
-        self.world = World(SCREEN_WIDTH,SCREEN_HEIGHT)
+    def setup(self): #for restart
+        self.world = World()
+        #character
         self.ninja = ModelSprite(self.setninja[0][0],model = self.world.ninja)
         self.sheild = ModelSprite(self.setsheild[0],model = self.world.sheild)
+        #item
         self.gensheild = ModelSprite('images/item4.png',model = self.world.gensheild)
         self.barrel = ModelSprite('images/item1.png',model = self.world.barrel)
         self.barrel2 = ModelSprite('images/item1.png',model = self.world.barrel2)
         self.shuriken = ModelSprite('Images/item2.png',model = self.world.shuriken1)
         self.knife = ModelSprite('images/item3.png',model = self.world.knife)
+        #text
         self.textscore = self.ReadScore()    
     def ReadScore(self):
         file = open("highscore.txt","r")
@@ -57,17 +63,19 @@ class NinninWindow(arcade.Window):
         file.write(str(self.world.score))
         file.close()
     def on_key_press(self, key, key_modifiers):
-        if(self.world.ninja.status == 1):
+        if(self.world.ninja.status == 1):  #gameplay
             self.world.on_key_press(key, key_modifiers)
-        elif(self.world.ninja.status == 0):
+        elif(self.world.ninja.status == 0): #gamerestart
             self.setup()
     def update(self,delta):
-        if(self.world.ninja.status == 1):
+        if(self.world.ninja.status == 1): #gameplay
             self.world.update(delta)
-            self.ninja = ModelSprite(self.setninja[self.world.flip][self.world.ninja.pic],model = self.world.ninja)
-            self.sheild = ModelSprite(self.setsheild[self.world.flip],model = self.world.sheild)
+            
+            self.ninja = ModelSprite(self.setninja[self.world.flip][self.world.ninja.pic],model = self.world.ninja) #change pic character
+            self.sheild = ModelSprite(self.setsheild[self.world.flip],model = self.world.sheild) #change pic sheild
+            #write score & highscore
             self.textscore = self.ReadScore()
-            if(self.world.score>int(self.textscore)):
+            if(self.world.score>int(self.textscore)): 
                 self.WriteScore()
     def drawgameplay(self):
         self.ninja.draw()
@@ -85,8 +93,9 @@ class NinninWindow(arcade.Window):
         arcade.draw_text(str("YourScore: "+str(self.world.score)),self.width - 270, self.height -500,arcade.color.BLUE, 15)
     def on_draw(self):
         arcade.start_render()
-        arcade.draw_texture_rectangle(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2,SCREEN_WIDTH, SCREEN_HEIGHT, self.background)
+        
         if(self.world.ninja.status == 1):
+            arcade.draw_texture_rectangle(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2,SCREEN_WIDTH, SCREEN_HEIGHT, self.background)
             self.drawgameplay()
         elif(self.world.ninja.status == 0):
             self.drawgameover()
